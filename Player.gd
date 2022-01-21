e2fb6742c599da2978b464d6479310e23b2766c1
Nathan Eloe <powerofazure@gmail.com>
2022-01-21 10:46:12 -0600

    Finalize Player init

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400
 var screen_size
 
@@ -7,6 +9,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -39,3 +42,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal('hit')
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
