ca6417a1ea66372513706cc6473ff0221dc1a1d8
Nathan Eloe <powerofazure@gmail.com>
2022-01-26 10:53:20 -0600

    Spawn enemies and hook up player

---
+++ Main.gd
@@ -0,0 +1,43 @@
+extends Node
+
+export (PackedScene) var Mob
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	new_game()
+
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+
+func _on_MobTimer_timeout():
+	$MobPath/MobSpawnLocation.offset = randi()
+	var mob = Mob.instance() 
+	add_child(mob)
+	var direction = $MobPath/MobSpawnLocation.rotation
+	mob.position = $MobPath/MobSpawnLocation.position
+	direction += PI / 2
+	direction += rand_range(-PI/4, PI/4)
+	mob.rotation = direction
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	
