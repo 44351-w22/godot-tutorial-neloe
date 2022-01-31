bae44deb1d5fa5d0b9d56bf72989567d9fd66275
Nathan Eloe <powerofazure@gmail.com>
2022-01-31 11:53:22 -0600

    Add HUD to main game

--- Main.gd
+++ Main.gd
@@ -6,18 +6,21 @@ var score
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	new_game()
+	#new_game()
 
 
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message('Get Ready')
 
 
 func _on_StartTimer_timeout():
@@ -27,6 +30,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_MobTimer_timeout():
